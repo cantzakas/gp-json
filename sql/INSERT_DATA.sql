e43b3c8bfb353f4c4094ea4c5465d56451aca9b3
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2018-10-10 09:34:26 +0100

    Update INSERT_DATA.sql

--- sql/INSERT_DATA.sql
+++ sql/INSERT_DATA.sql
@@ -34,79 +34,79 @@ DELETE FROM json_quicklz_blocksize8K;
 INSERT INTO json_quicklz_blocksize8K
 	SELECT array_to_json(array_agg(col1)) # produces rows of size 7,855 - 8,100 bytes
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%300 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%300 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_quicklz_blocksize16K;
 INSERT INTO json_quicklz_blocksize16K
 	SELECT array_to_json(array_agg(col1))  # produces rows of size 15,808 - 16,065 bytes
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%150 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%150 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_quicklz_blocksize32K;
 INSERT INTO json_quicklz_blocksize32K
 	SELECT array_to_json(array_agg(col1)) # produces rows of size 31,692 - 32,037 bytes
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%75 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%75 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_zlib1_blocksize8K;
 INSERT INTO json_zlib1_blocksize8K
 	SELECT array_to_json(array_agg(col1))
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%300 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%300 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_zlib1_blocksize16K;
 INSERT INTO json_zlib1_blocksize16K
 	SELECT array_to_json(array_agg(col1))
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%150 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%150 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_zlib1_blocksize32K;
 INSERT INTO json_zlib1_blocksize32K
 	SELECT array_to_json(array_agg(col1))
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%75 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%75 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_rle4_blocksize8K;
 INSERT INTO json_rle4_blocksize8K
 	SELECT array_to_json(array_agg(col1))
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%300 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%300 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_rle4_blocksize16K;
 INSERT INTO json_rle4_blocksize16K
 	SELECT array_to_json(array_agg(col1))
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%150 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%150 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
 
 DELETE FROM json_rle4_blocksize32K;
 INSERT INTO json_rle4_blocksize32K
 	SELECT array_to_json(array_agg(col1))
 	FROM (
-		SELECT col1::text, ((col1->>'id')::int)%75 AS modulo150_result
+		SELECT col1::text, ((col1->>'id')::int)%75 AS modulo_result
 		FROM json_standard
 	) A
-	GROUP BY modulo150_result;
+	GROUP BY modulo_result;
